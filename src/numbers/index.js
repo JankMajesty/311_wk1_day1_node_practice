87ae3e1c84eab6aeaa1c7905225bd020235dfc52
JankMajesty <lmcgill36@gmail.com>
2021-07-04 14:19:19 -0500

    pushing code

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,17 +1,31 @@
 const isEven = (num) => {
-  // write code for numbers.isEven
-
+  if (num % 2 === 0){
+    return true
+  } else { return false }
 }
+console.log(isEven)
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  let sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue)
+  return sum
 }
+
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
+  for (let i = 0; i < arr.length; i++){
+    for (let j = 0; j < arr.length; j++){
+      if (arr[i] + arr[j] === sum) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
 
 }
+
 
 module.exports = {
   isEven,
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,22 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  let string = str.split(delim)
+  return string
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  let arrayPairs = [];
+  for (let i = 0; i < str.length; i +=2) {
+    arrayPairs.push(str.substring(i, i+2));
+  }
+  return arrayPairs;
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let revArr = str.split('')
+  return revArr.reverse().join('')
 }
 
 module.exports = {
